199f0c2addb37fda495df7ad85dfb11d1ce77087
Saul <saul292.atmes@yahoo.com>
2017-11-18 21:02:30 -0500

    touched up some of the averaging data structures

--- cloud/dashboard.py
+++ cloud/dashboard.py
@@ -14,9 +14,9 @@ import logging.handlers
 
 APP = flask.Flask(__name__)
 statisticstimestamp=""
-averages={"co2":0,"temperature":0,"light":0,"humidity":0}
+averages={"temperature":0,"speed":0,'rpm':0}
 averagecount=0
-runningaverages={"co2":0,"temperature":0,"light":0,"humidity":0}
+runningaverages={"temperature":0,"speed":0,"rpm":0}
 
 
 
@@ -38,41 +38,37 @@ def get_Data():
         
         response = table.scan(FilterExpression=Key('Timestamp').between(timestampold, timestamp))
         if(statisticstimestamp<(time.time()-60)):
-            averages["co2"]=float(runningaverages["co2"])/averagecount
-            runningaverages["co2"]=0
             averages["temperature"]=float(runningaverages["temperature"])/averagecount
             runningaverages["temperature"]=0
-            averages["light"]=float(runningaverages["light"])/averagecount
-            runningaverages["light"]=0
-            averages["humidity"]=float(runningaverages["humidity"])/averagecount
-            runningaverages["humidity"]=0
+            averages["speed"]=float(runningaverages["speed"])/averagecount
+            runningaverages["speed"]=0
+            averages["rpm"]=float(runningaverages["rpm"])/averagecount
+            runningaverages["rpm"]=0
             statisticstimestamp=time.time()
             averagecount=0
             print(averages)
         items = response['Items']
             if len(items) > 0:
             averagecount+=1
-            runningaverages["co2"]+=int(items[0]["co2"])
-            runningaverages["humidity"]+=int(items[0]["humidity"])
-            runningaverages["light"]+=int(items[0]["light"])
+            runningaverages["speed"]+=int(items[0]["speed"])
             runningaverages["temperature"]+=int(items[0]["temperature"])
+            runningaverages["rpm"]+=int(items[0]["rpm"])
             data["Timestamp"] = str(items[0]["Timestamp"])
-            data["co2"] = int(items[0]["co2"])
             data["temperature"] = int(items[0]["temperature"])
-            data["light"] = int(items[0]["light"])
-            data["humidity"] = int(items[0]["humidity"])
+            data["speed"] = int(items[0]["speed"])
+            data["rpm"] = int(items[0]["rpm"])
             data["co2avg"] = float(averages["co2"])
-                        data["temperatureavg"] = float(averages["temperature"])
-                        data["lightavg"] = float(averages["light"])
-                        data["humidityavg"] = float(averages["humidity"])
+            data["temperatureavg"] = float(averages["temperature"])
+            data["speedaverage"] = float(averages["speed"])
+            data["rpmaverage"] = float(averages["rpm"])
             logger.info(json.dumps(data))
             return jsonify(json.dumps(data))
 
         else:
-            return jsonify({'temperature': 0, 'humidity': 0,'co2': 0,'light': 0})
+            return jsonify({'temperature': 0, 'speed': 0,'rpm':0})
 
     except Exception as err:
         print("Unexpected error:", err)
         pass
             
-    return jsonify({'temperature': 0, 'humidity': 0,'co2': 0,'light': 0})
+    return jsonify({'temperature': 0, 'speed': 0,'rpm':0})
