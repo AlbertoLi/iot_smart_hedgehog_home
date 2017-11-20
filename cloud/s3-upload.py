8ac77ccfb1f55c2e38a693fc5df1e540fa5577b1
Saul <saul292.atmes@yahoo.com>
2017-11-19 20:52:39 -0500

    added camera code

--- cloud/s3-upload.py
+++ cloud/s3-upload.py
@@ -7,9 +7,11 @@ BUCKET = "iot-smart-hedgehog-home-bucket"
 s3 = boto3.client('s3', aws_access_key_id=AWS_KEY,
                             aws_secret_access_key=AWS_SECRET)
                             
+while(True):
+    subprocess.call(["raspistill", "-o","pygmy_hedgehogs_test.jpg"])
+    filenameWithPath = "./pygmy_hedgehogs_test.jpg"                            
+    path_filename='pygmy_hedgehogs_test.jpg'
 
-filenameWithPath = "./pygmy_hedgehogs_test.jpg"                            
-path_filename='pygmy_hedgehogs_test.jpg'
-
-s3.upload_file(filenameWithPath, BUCKET, path_filename)
-s3.put_object_acl(ACL='public-read', Bucket=BUCKET, Key=path_filename)
+    s3.upload_file(filenameWithPath, BUCKET, path_filename)
+    s3.put_object_acl(ACL='public-read', Bucket=BUCKET, Key=path_filename)
+    time.sleep(5)
