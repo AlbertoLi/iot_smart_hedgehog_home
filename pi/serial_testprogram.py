44e8d1f88953ee1c7787f6feed1fee8f87ebbd4d
Saul <saul292.atmes@yahoo.com>
2017-11-26 16:50:20 -0500

    changed serial program to use subprocesses

--- pi/serial_testprogram.py
+++ pi/serial_testprogram.py
@@ -29,39 +29,9 @@ while 1 :
         print message.body
         ######## Add code here to send thru serial. Use 'data' variable which is in json form ########
         if (data[0] == "Music"):
-            input = "m"
+            subprocess.call(["sudo echo","m >",  "/dev/ttyACM0"])
         elif (data[0] == "Snack"):
-            input = "t"
-        else:
-            input = None
-
-        ######## Add code here to send thru serial. Use 'data' variable which is in json form ########
-        message.delete()
-        time.sleep(0.7)
-
-        if input == None:
-            while ser.inWaiting() > 0:
-                out += ser.read(22)
-
-            if out != '':
-                print ">>" + out
-        else:
-            # send the character to the device
-            # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
-            ser.write(input + '\r\n')
-            out = ''
-            # let's wait one second before reading output (let's give device time to answer)
-            time.sleep(1)
-            while ser.inWaiting() > 0:
-                out += ser.read(22)
-
-            if out != '':
-                print ">>" + out
-    if input == None:
-            while ser.inWaiting() > 0:
-                out += ser.read(22)
-
-            if out != '':
-                print ">>" + out
+            subprocess.call(["sudo echo", "t >", "/dev/ttyACM0"])
+        
 
 
