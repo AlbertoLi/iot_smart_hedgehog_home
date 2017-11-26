993daad591eb14f017e9471a260cd016c1b8dfd6
unknown <mtatum7@gatech.edu>
2017-11-26 18:36:32 -0500

    main.cpp update

--- 4180_final/main.cpp
+++ 4180_final/main.cpp
@@ -18,8 +18,9 @@ wave_player waver(&DACout);
 volatile float temp_out;
 volatile float rpm_out;
 volatile float wheel_speed_out;
-volatile int treat=0;
-volatile int music=0;
+volatile int treat = 0;
+volatile int music = 0;
+volatile bool x;
 
 DigitalOut myled1(LED1);
 
@@ -31,39 +32,39 @@ InterruptIn risingEdge(p11);
 
 void dev_recv()
 {
-    char temp = 0;
-    myled1 = !myled1;
-    while(pi.readable()) {
-        temp = pi.getc();
-        if (temp=='t') treat=1;
-        if (temp=='m') music=1;
-    }
+	char temp = 0;
+	myled1 = !myled1;
+	while (pi.readable()) {
+		temp = pi.getc();
+		if (temp == 't') treat = 1;
+		if (temp == 'm') music = 1;
+	}
 }
 
 void check_temp() {
-    float tempC, tempF;
-    while(1) {
-        //conversion to degrees C - from sensor output voltage per LM61 data sheet
-        tempC = ((LM61*3.3)-0.600)*100.0;
-        //convert to degrees F
-        tempF = (9.0*tempC)/5.0 + 32.0;
-        temp_out = tempF+16.7;
-        //print current temp
-        Thread::wait(500);
-    }    
+	float tempC, tempF;
+	while (1) {
+		//conversion to degrees C - from sensor output voltage per LM61 data sheet
+		tempC = ((LM61*3.3) - 0.600)*100.0;
+		//convert to degrees F
+		tempF = (9.0*tempC) / 5.0 + 32.0;
+		temp_out = tempF + 16.7;
+		//print current temp
+		Thread::wait(500);
+	}
 }
 
 void deliver_snack()
 {
-    if(treat=1){
-        myservo = 1; //closed position
-        Thread::wait(1000);
-        myservo = .7; //open position
-        Thread::wait(200); // open for .2 secs delivers half a small tupperware
-        myservo = 1;
-        Thread::wait(500);
-        treat=0;
-    }    
+	if (treat = 1) {
+		myservo = 1; //closed position
+		Thread::wait(1000);
+		myservo = .7; //open position
+		Thread::wait(200); // open for .2 secs delivers half a small tupperware
+		myservo = 1;
+		Thread::wait(500);
+		treat = 0;
+	}
 }
 
 
@@ -71,62 +72,64 @@ void deliver_snack()
 
 char* getOut()
 {
-    char output[22];
+	char output[22];
 
-    snprintf(output, 22, "%f3.1, %f4.0, %f1.5", temp_out, rpm_out, wheel_speed_out);
-    return output;
+	snprintf(output, 22, "%f3.1, %f4.0, %f1.5", temp_out, rpm_out, wheel_speed_out);
+	return output;
 }
 
 void send_data() {
-    while(1)
-    {
-        pi.puts(getOut());
-        Thread::wait(5000);
-    }
+	while (1)
+	{
+		pi.puts(getOut());
+		Thread::wait(5000);
+	}
 }
 
 void pulses() {
-    count++;
+	count++;
+	x = true;
 }
 
 void check_wheel() {
-    risingEdge.rise(&pulses);
-    while (1) {
-        t.reset();
-        t.start();
-        count = 0;
-        while (t.read_ms() < 1001) {
-            ;
-        }
-        t.stop();
-        long int temp = count;
-        double circumference = 0.06 * 3.1416; // 6 cm wheel diameter * pi 
-        double rev = (double)temp;
-        double rpm = rev * 60;
-        double speed = circumference * rev;
-        rpm_out = (float) rpm;
-        wheel_speed_out = (float) speed;
-    }
+	count = 0;
+	double rpm = 0;
+	double speed = 0;
+	x = false;
+	double circumference = 0.266 * 3.1416; // 26. cm wheel diameter * pi 
+	while (1) {
+		t.start();
+		risingEdge.rise(&pulses);
+		if (x) {
+			t.stop();
+			rpm = (double)60 / t.read();
+			speed = (double)circumference / t.read();
+			t.reset();
+			x = false;
+		}
+		rpm_out = (float)rpm;
+		wheel_speed_out = (float)speed;
+		Thread::wait(1000);
+	}
 }
 
 
 int main() {
-    //printf("Hello, in Main");
-    Thread t1(check_temp);
-    Thread t2(send_data);
-    Thread t3(check_wheel);
-    
-    pi.baud(9600);
-    pi.attach(&dev_recv, Serial::RxIrq);
-    
-    while (1) {
-        if(music==1) {
-            FILE *wave_file = fopen("/sd/wavfiles/crickets.wav", "r");
-            waver.play(wave_file);
-            fclose(wave_file);
-            Thread::wait(1000);
-            music=0;
-        }
-    }   
-}
+	//printf("Hello, in Main");
+	Thread t1(check_temp);
+	Thread t2(send_data);
+	Thread t3(check_wheel);
 
+	pi.baud(9600);
+	pi.attach(&dev_recv, Serial::RxIrq);
+
+	while (1) {
+		if (music == 1) {
+			FILE *wave_file = fopen("/sd/wavfiles/crickets.wav", "r");
+			waver.play(wave_file);
+			fclose(wave_file);
+			Thread::wait(1000);
+			music = 0;
+		}
+	}
+}
